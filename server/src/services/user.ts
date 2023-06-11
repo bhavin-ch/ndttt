4834b0e67a630de16223a66cd5ae152ec66ba71a
Bhavin <bhavin137@gmail.com>
2023-06-11 17:27:03 +0200

    Use prisma generated types

--- server/src/services/user.ts
+++ server/src/services/user.ts
@@ -1,15 +1,16 @@
 import { Prisma, PrismaClient } from '@prisma/client';
 import { createUserInput } from '../types';
 import { generateHash } from '../utils/auth';
+import { User } from '@prisma/client';
 
-let User :Prisma.UserDelegate<unknown>;
+let UserModel: Prisma.UserDelegate<unknown>;
 
-export const initUsersService = (client: PrismaClient) => User = client.user;
+export const initUsersService = (client: PrismaClient) => UserModel = client.user;
 
-export const createUser = async (input: createUserInput) => {
+export const createUser = async (input: createUserInput): Promise<User> => {
   const { password, email, username, name } = input;
   const passwordHash = await generateHash(password);
-  const user = await User.create({
+  const user = await UserModel.create({
     data: {
       email,
       username,
@@ -20,12 +21,12 @@ export const createUser = async (input: createUserInput) => {
   return user;
 };
 
-export const getAllUsers = async () => {
-  return await User.findMany();
+export const getAllUsers = async (): Promise<User[]> => {
+  return await UserModel.findMany();
 };
 
-export const getUserById = async (id: string) => {
-  return await User.findUnique({
+export const getUserById = async (id: string): Promise<User> => {
+  return await UserModel.findUnique({
     where: {
       id,
     }
